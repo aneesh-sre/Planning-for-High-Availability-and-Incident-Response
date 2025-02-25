9267cab05bc06479817171ab7800636d5a96bbfe
aneesh-sre <aneesh.ashok@natwest.com>
2025-02-25 15:13:21 +0000

    Update _config.tf

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-aneesh" # Replace it with your S3 bucket name
+     bucket = "udacity-tf-aneeshsre" # Replace it with your S3 bucket name
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
