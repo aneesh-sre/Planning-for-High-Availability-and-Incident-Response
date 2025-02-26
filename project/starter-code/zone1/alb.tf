44b63f9979d266b96ee4929456276955577ee214
aneesh-sre <aneesh.ashok@natwest.com>
2025-02-26 01:10:38 +0000

    Update alb.tf

--- project/starter-code/zone1/alb.tf
+++ project/starter-code/zone1/alb.tf
@@ -1,6 +1,6 @@
 module "project_alb" {
   source            = "./modules/alb"
-  ec2               = module.project_ec2.instance_ids  
+  ec2               = module.project_ec2.instance 
   ec2_sg            = module.project_ec2.ec2_sg  
   subnet_ids        = module.vpc.public_subnet_ids  
   vpc_id           = module.vpc.vpc_id
